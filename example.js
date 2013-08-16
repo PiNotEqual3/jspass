c9b709ef7a71328fabf71905a483e76f1028d9aa
Pi <pi@cartiss.de>
2013-08-16 14:52:32 +0200

    PGP Demo: - generate key pair - encrypt - decrypt

--- example.js
+++ example.js
@@ -1,39 +1,5 @@
 /*
-generate key
- var key = openpgp.generate_key_pair(1, 1024, "Mr. Tester <test@test.de>")
- //$('#message').val(openpgp.write_encrypted_message(openpgp.read_publicKey(key.publicKeyArmored), $('#message').val()));
- var msg = openpgp.write_encrypted_message(openpgp.read_publicKey(key.publicKeyArmored), 'test message');
 
- openpgp.read_privateKey(key.privateKeyArmored);
- var priv_key = openpgp.read_privateKey(key.privateKeyArmored);
-
-
- var keymat = null;
- var sesskey = null;
- // Find the private (sub)key for the session key of the message
- for (var i = 0; i< msg[0].sessionKeys.length; i++) {
- if (priv_key[0].privateKeyPacket.publicKey.getKeyId() == msg[0].sessionKeys[i].keyId.bytes) {
- keymat = { key: priv_key[0], keymaterial: priv_key[0].privateKeyPacket};
- sesskey = msg[0].sessionKeys[i];
- break;
- }
- for (var j = 0; j < priv_key[0].subKeys.length; j++) {
- if (priv_key[0].subKeys[j].publicKey.getKeyId() == msg[0].sessionKeys[i].keyId.bytes) {
- keymat = { key: priv_key[0], keymaterial: priv_key[0].subKeys[j]};
- sesskey = msg[0].sessionKeys[i];
- break;
- }
- }
- }
- if (keymat != null) {
- if (!keymat.keymaterial.decryptSecretMPIs($('#decpassword').val())) {
- console.log("Password for secrect key was incorrect!");
- return;
- }
- console.log(msg[0].decrypt(keymat, sesskey));
- } else {
- console.log("No private key found!");
- }
 
 
  */
@@ -44,33 +10,85 @@ function showMessages(text)
     console.log(text);
 }
 
-function encrypt() {
-  if (window.crypto.getRandomValues) {
-    //require("./openpgp.min.js");
-    openpgp.init();
+function init()
+{
+    if (window.crypto.getRandomValues) {
+        //require("./openpgp.min.js");
+        openpgp.init();
 
-
-
-  } else {
-    $("#mybutton").val("browser not supported");
-    window.alert("Error: Browser not supported\nReason: We need a cryptographically secure PRNG to be implemented (i.e. the window.crypto method)\nSolution: Use Chrome >= 11, Safari >= 3.1 or Firefox >= 21");   
-    return false;
-  }
+    } else {
+        $("#mybutton").val("browser not supported");
+        window.alert("Error: Browser not supported\nReason: We need a cryptographically secure PRNG to be implemented (i.e. the window.crypto method)\nSolution: Use Chrome >= 11, Safari >= 3.1 or Firefox >= 21");
+        return false;
+    }
 }
-$('#encrypt-btn').click( encrypt );
-$('#decrypt-btn').click( function(){
-    openpgp.init();
 
+function encrypt(message) {
+    // read keys
+    var key = get_key();
 
-} );
+    // encrypt
+    var msg_crypted = openpgp.write_encrypted_message(openpgp.read_publicKey(key.publicKeyArmored), message);
 
-function generate_keys()
+    return msg_crypted;
+}
+$('#encrypt-btn').click( function(){
+    var msg_crypted = encrypt($('#message').val());
+    $('#message').val(msg_crypted);
+});
+
+function decrypt(msg_crypted)
+{
+    // read keys
+    var key = get_key();
+    var priv_key = openpgp.read_privateKey(key.privateKeyArmored);
+
+    var msg = openpgp.read_message(msg_crypted);
+    var keymat = null;
+    var sesskey = null;
+
+    // Find the private (sub)key for the session key of the message
+    for (var i = 0; i< msg[0].sessionKeys.length; i++) {
+        if (priv_key[0].privateKeyPacket.publicKey.getKeyId() == msg[0].sessionKeys[i].keyId.bytes) {
+            keymat = { key: priv_key[0], keymaterial: priv_key[0].privateKeyPacket};
+            sesskey = msg[0].sessionKeys[i];
+            break;
+        }
+        for (var j = 0; j < priv_key[0].subKeys.length; j++) {
+            if (priv_key[0].subKeys[j].publicKey.getKeyId() == msg[0].sessionKeys[i].keyId.bytes) {
+                keymat = { key: priv_key[0], keymaterial: priv_key[0].subKeys[j]};
+                sesskey = msg[0].sessionKeys[i];
+                break;
+            }
+        }
+    }
+    if (keymat != null) {
+        if (!keymat.keymaterial.decryptSecretMPIs($('#decpassword').val())) {
+            console.log("Password for secrect key was incorrect!");
+            return;
+        }
+        return msg[0].decrypt(keymat, sesskey);
+    } else {
+        console.log("No private key found!");
+    }
+}
+$('#decrypt-btn').click( function()
+{
+    var message = decrypt( $('#message').val() );
+    $('#message').val( message );
+});
+
+function generate_keys(user)
 {
     //generate key
-    var key = openpgp.generate_key_pair(1, 1024, "Mr. Tester <test@test.de>");
+    var key = openpgp.generate_key_pair(1, 1024, user);
     $('#pub_key').val( key.publicKeyArmored );
     $('#priv_key').val( key.privateKeyArmored );
 }
+$('#gen_keys').click( function()
+{
+    generate_keys( $('#name').val() + " <" + $('#email').val() + ">" );
+});
 
 function get_key()
 {
@@ -84,19 +102,16 @@ function get_key()
 pgptest();
 function pgptest()
 {
-    openpgp.init();
+    init();
 
-    generate_keys();
+    generate_keys("Mr. Tester <test@test.de>");
 
     // read keys
     var key = get_key();
 
-    // encrypt
-    var msg_crypted = openpgp.write_encrypted_message(openpgp.read_publicKey(key.publicKeyArmored), 'test message');
-    var priv_key = openpgp.read_privateKey(key.privateKeyArmored);
+    var msg_crypted = encrypt('test message');
 
-    //console.log(msg_crypted);
-    //console.log(key.privateKeyArmored);
+    var priv_key = openpgp.read_privateKey(key.privateKeyArmored);
 
     // decrypt
     var msg = openpgp.read_message(msg_crypted);
